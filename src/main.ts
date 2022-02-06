8cd0bf30c0b1f8426be2c6e807c68daca680d8c7
juhito <Jussi8@hotmail.com>
2022-02-06 20:50:17 +0200

    start emu

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { Emulator } from "./emulator";
+import * as fs from "fs";
+import * as path from "path";
+
+function findRom(romPath: string): Uint8Array {
+    return fs.readFileSync(path.resolve(romPath));
+}
+const emu: Emulator = new Emulator();
+emu.load(findRom("rom/ibm_logo.ch8"));
+
 
 function start(): void {
-    const emu: Emulator = new Emulator();
+    emu.tick();
+
+    setTimeout(start, 500);
 }
 
 start();
