08220adb5c9aa6e59da632fffe6558222ff332cd
juhito <Jussi8@hotmail.com>
2022-02-06 21:01:32 +0200

    fix rendering bug, should access screen [y][x] not [x][y]

--- src/screen.ts
+++ src/screen.ts
@@ -2,43 +2,35 @@ import * as constants from "./utils/constants";
 import * as blessed from "blessed";
 
 export class Screen {
-    private _screen: Array<Array<number>>;
-    private _terminal: blessed.Widgets.Screen;
+    private screen: Array<Array<number>>;
+    private terminal: blessed.Widgets.Screen;
 
     constructor() {
-        this._screen = new Array<Array<number>>();
-        this._init_screen();
+        this.screen = new Array<Array<number>>();
+        this.init_screen();
 
-        this._terminal = blessed.screen({smartCSR: true});
+        this.terminal = blessed.screen({smartCSR: true});
     }
 
     public drawPixel(x: number, y: number): boolean {
-        this._screen[x][y] ^= 1;
+        this.screen[y][x] ^= 1;
 
-        if(this._screen[x][y]) {
-            this._terminal.fillRegion('#ffffff', 'X', x, x + 1, y, y + 1);
+        if(this.screen[y][x]) {
+            this.terminal.fillRegion('', '<', x, x + 1, y, y + 1);
         }
         else {
-            this._terminal.clearRegion(x, x + 1, y, y + 1);
+            this.terminal.clearRegion(x, x + 1, y, y + 1);
         }
 
-        this._terminal.render();
-        return !this._screen[x][y];
-    }
-
-    public render(): void {
-        for(let i: number = 0; i < this._screen.length; i++) {
-            for(let j: number = 0; j < this._screen[i].length; j++) {
-                console.log(this._screen[i][j]);
-            }
-        }
+        this.terminal.render();
+        return !this.screen[y][x];
     }
     
-    private _init_screen(): void {
+    private init_screen(): void {
         for (let i: number = 0; i < 32; i++) {
-            this._screen.push([]);
+            this.screen.push([]);
             for (let j: number = 0; j < 64; j++) {
-                this._screen[i].push(0);
+                this.screen[i].push(0);
             }
         }
     }
